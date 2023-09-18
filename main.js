6d4778f0c9cda3ac5d604448b28fa7d555e1a33b
luizatorquatto <133678662+luizatorquatto@users.noreply.github.com>
2023-09-18 12:40:05 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+function pom()   {
+    alert("botão pom clicado");
+}
+document.querySelector.toString('.tecla_pom').onclik = pom;
+
+function clap()   {
+    alert("botão clap clicado")
+}
+document.querySelector('.tecla_clap').onclik =  clap;
